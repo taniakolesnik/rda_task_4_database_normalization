ee517f32393ba146b6d46f0bbc338a88121e28f1
Tania Kolesnik <kolesnik@gmail.com>
2025-12-10 17:14:08 +0000

    update task.sql

--- task.sql
+++ task.sql
@@ -12,7 +12,14 @@ CREATE TABLE Countries (
 CREATE TABLE ProductInventory (
     ID INT,
     ProductName VARCHAR(50),
+    WarehouseID INT,
     WarehouseAmount INT,
+	FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Warehouse (
+    ID INT,
     WarehouseName VARCHAR(50),
     WarehouseAddress VARCHAR(50), 
     CountryID INT,
@@ -26,8 +33,14 @@ INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
+
+INSERT INTO Warehouse (ID,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (1, 'Warehouse-1', 'City-1, Street-1',1);
+
+INSERT INTO Warehouse (ID,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (2, 'Warehouse-2', 'City-2, Street-2',2);
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID,ProductName,WarehouseID, WarehouseAmount)
+	VALUES (1, 'AwersomeProduct', 1, 1);
+INSERT INTO ProductInventory (ID,ProductName,WarehouseID, WarehouseAmount)
+	VALUES (2, 'AwersomeProduct', 2, 5);
